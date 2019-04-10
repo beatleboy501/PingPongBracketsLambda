4e327fc34096416637fd02df27dda06652d3043a
Andrew Allison <aallis@amazon.com>
2019-04-10 17:15:10 -0400

    adding longer timeout and template to build/deploy all functions

---
+++ deployLambdas.sh
@@ -0,0 +1,6 @@
+sam build -t ./src/create-bracket/template.yaml -u
+sam build -t ./src/create-result/template.yaml -u
+sam build -t ./src/get-bracket-by-id/template.yaml -u
+sam build -t ./src/get-brackets-for-user/template.yaml -u
+sam build -t ./src/get-cognito-user-by-id/template.yaml -u
+sam build -t ./src/get-cognito-users/template.yaml -u
